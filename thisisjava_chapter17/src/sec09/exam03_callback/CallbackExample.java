ce672d1321605308674674b049488afaf26adfe6
JungHyun <freenham@gmail.com>
2017-07-24 14:27:18 +0900

    12.9 스레드풀(4)

---
+++ thisisjava_chapter17/src/sec09/exam03_callback/CallbackExample.java
@@ -0,0 +1,17 @@
+package sec09.exam03_callback;
+
+import java.util.concurrent.ExecutorService;
+
+public class CallbackExample {
+	private ExecutorService executorService;
+	
+	public CallbackExample() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
